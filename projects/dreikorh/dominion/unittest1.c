642e445a9525395d199e886a7805db2ebee21528
HDreikorn <hillary@dreikorn.com>
2019-04-28 11:17:41 -0700

    starting unittest1 suite

---
+++ projects/dreikorh/dominion/unittest1.c
@@ -0,0 +1,32 @@
+/*
+* UNIT TEST 1: testing refactored adventurer action.
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+asserttrue(){
+
+}
+
+int main() {
+    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+    int game, seed = 1000, player = 0, numPlayers = 2, handCount = 0;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G; 
+
+    game = initializeGame(numPlayers, k, seed, &G);
+    handCount = G.handCount;
+    cardEffect(adventurer, choice1, choice2, choice3, &G, handpos, &bonus);
+
+    // Test 1: Test if hand count has increased by 2.
+    printf("G.handCount = %d, Expected = %d\n", G.handCount, handCount+2);
+
+
+    return 0;
+}
