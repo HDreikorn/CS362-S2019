587dd6de344f7d1cec73664dd4814bce33402534
HDreikorn <hillary@dreikorn.com>
2019-04-28 13:09:08 -0700

    added test 2

--- projects/dreikorh/dominion/unittest1.c
+++ projects/dreikorh/dominion/unittest1.c
@@ -9,20 +9,32 @@
 #include <assert.h>
 #include "rngs.h"
 
+#define MY_ASSERT(condition, msg)\
+	if (condition)\
+		printf("PASSED\n"); \
+	else \
+		printf(msg);
+
 int main() {
     int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-    int game, seed = 1000, player = 0, numPlayers = 2, handCount = 0;
+    int game, seed = 1, player = 0, numPlayers = 4, handCount = 0;
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
     struct gameState G; 
 
     game = initializeGame(numPlayers, k, seed, &G);
+	assert(game == 0);
     handCount = numHandCards(&G);
-    //cardEffect(adventurer, choice1, choice2, choice3, &G, handpos, &bonus);
+    game = cardEffect(adventurer, choice1, choice2, choice3, &G, handpos, &bonus);
 
-    // Test 1: Test if hand count has increased by 2.
-    //printf("G.handCount = %d, Expected = %d\n", numHandCards(&G), handCount+2);
+    // Test 1: Test if hand count has increased by 2, checking for exactly 2 also ensures other cards were discarded.
+	printf("TEST 1: ");
+	MY_ASSERT(numHandCards(&G) == (handCount+2), "\"FAILED: G.handCount = %d, Expected = %d\n\", numHandCards(&G), handCount+2");
 
-
-    return 0;
+	// Test 2: Test if last card is a treasure card.
+	printf("TEST 2: ");
+    MY_ASSERT((G.hand[player][numHandCards(&G)-1] == 4) ||
+			  (G.hand[player][numHandCards(&G)-1] == 5) ||
+			  (G.hand[player][numHandCards(&G)-1] == 6), "\"FAILED - Last card is %d, not treasure\",G.hand[player][numHandCards(&G)-1]");
+	return 0;
 }
