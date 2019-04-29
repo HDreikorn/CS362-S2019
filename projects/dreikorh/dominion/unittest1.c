224fbbf826bed2d7ae4bbbeb299e937649a1af5f
HDreikorn <hillary@dreikorn.com>
2019-04-29 09:07:54 -0700

    adding test 5

--- projects/dreikorh/dominion/unittest1.c
+++ projects/dreikorh/dominion/unittest1.c
@@ -21,16 +21,29 @@ int main() {
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
     int startingSupplyCount[treasure_map+1] = {0};
+	int p1FullDeck[3], p2FullDeck[3];
     struct gameState G; 
 
 	printf("Starting Unit Test 1 suite...\n");
     game = initializeGame(numPlayers, k, seed, &G);
     assert(game == 0);
 	printf("Game initialized successfully for testing...\n");
-    // Capture before states of handCount, victory cards, kingdom cards
+    // Capture before states of handCount, victory cards, kingdom cards, and playershand, deck, discard count.
     handCount = numHandCards(&G);
 	for(i=0; i < 17; i++) {
 		startingSupplyCount[i] = G.supplyCount[i];
+	}
+
+	// All players, but player 0 who starts
+	p1FullDeck[0] = G.handCount[1];
+	p2FullDeck[0] = G.handCount[2];
+	p1FullDeck[1] = G.deckCount[1];
+	p2FullDeck[1] = G.deckCount[2];
+	p1FullDeck[2] = G.discardCount[1];
+	p2FullDeck[2] = G.discardCount[2];
+	for(i=0; i<3;i++){
+		printf("Player 2: %d\n", p1FullDeck[i]);
+		printf("Player 3: %d\n", p2FullDeck[i]);
 	}
 
     // Run cardEffect to test refactored adventurer action.
@@ -38,7 +51,7 @@ int main() {
 
     // Test 1: Test if hand count has increased by 2, checking for exactly 2 also ensures other cards were discarded.
 	printf("TEST 1: ");
-	MY_ASSERT(numHandCards(&G) == (handCount+2), "\"FAILED - G.handCount = %d, Expected = %d\n\", int(numHandCards(&G)), handCount+2");
+	MY_ASSERT(numHandCards(&G) == (handCount+2), "\"FAILED - G.handCount = %d, Expected = %d\n\", numHandCards(&G), handCount+2");
 
 	// Test 2: Test if last card is a treasure card.
 	printf("TEST 2: ");
@@ -53,7 +66,7 @@ int main() {
 			  (G.hand[player][numHandCards(&G)-2] == 6), "\"FAILED - Last card is %d, not treasure\",G.hand[player][numHandCards(&G)-2]");
 	
 	// Test 4: Test to make sure no changes were made to victory cards or kingdom cards
-    printf("Test 4:");
+    printf("Test 4: ");
     for (i=0; i <6; i++) {
 		if (startingSupplyCount[i] != G.supplyCount[i]){
 			printf("FAILED - Unexpected change made in card %d", i);
@@ -61,6 +74,9 @@ int main() {
 		}
 	}
 	printf("PASSED\n");
+
+	// Test 5: Test to make sure no changes were made to every other player's deck.
+	printf("Test 5: ");
 	
 	printf("Unit Test 1 complete.\n");
 
